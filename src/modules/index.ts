bbe6dcc70c83653897c5a0d4e2faf3cd1a81351b
taeryu7 <pegasy02@gmail.com>
2023-03-17 11:22:43 +0900

    0317_before

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,4 +1,20 @@
+import { combineReducers } from 'redux';
+import counter from './counter';
+import todos from './todos';
 
+const rootReducer = combineReducers({
+  counter,
+  todos
+});
+
+// 루트 리듀서를 내보내주세요.
+export default rootReducer;
+
+// 루트 리듀서의 반환값를 유추해줍니다
+// 추후 이 타입을 컨테이너 컴포넌트에서 불러와서 사용해야 하므로 내보내줍니다.
+export type RootState = ReturnType<typeof rootReducer>;
+
+/*
 // 카운터 리덕스 모듈 작성하기
 import { combineReducers } from 'redux';
 import counter from './counter';
@@ -13,3 +29,4 @@ export default rootReducer;
 // 루트 리듀서의 반환값를 유추해준다.
 // 추후 이 타입을 컨테이너 컴포넌트에서 불러와서 사용해야 하므로 내보내준다.
 export type RootState = ReturnType<typeof rootReducer>;
+*/
